9553b81cb622fc37aac60df7376d8a28a6572715
김민수 <kim112mgxld@gmail.com>
2020-07-20 18:28:19 +0900

    Create Title.svelte

---
+++ src/components/Title.svelte
@@ -0,0 +1,26 @@
+<div class="todo-list-title">
+    <h1>{title}</h1>
+    <slot name="subtitle"></slot>
+</div>
+
+<script>
+    export let title = ''
+</script>
+
+<style>
+    h1 {
+        color: orange;
+        font-size: 4em;
+
+        margin-bottom: 10px;
+    }
+
+    .todo-list-title {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        justify-content: center;
+
+        margin-bottom: 20px;
+    }
+</style>
